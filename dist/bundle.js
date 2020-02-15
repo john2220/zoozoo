9fdf97d11c230e44027d99bcccaac292bb37975e
John Raptis <jraptis@loceye.io>
2020-02-15 21:44:09 +0200

    first

---
+++ dist/bundle.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+function Button(_ref) {
+  var text = _ref.text;
+  return React.createElement("button", null, text);
+}
+
+exports.Button = Button;
